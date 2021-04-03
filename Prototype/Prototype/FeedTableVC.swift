eb1fb33ab60d2771dd5e179a3629a9f793886636
grav0211 <wbiosd111@gmail.com>
2021-04-03 12:22:41 -0400

    Add FeedTableVC and link to storyboard.

---
+++ Prototype/Prototype/FeedTableVC.swift
@@ -0,0 +1,22 @@
+//
+//  FeedVTableVC.swift
+//  Prototype
+//
+//  Created by Alexandre Gravelle on 2021-04-03.
+//
+
+import UIKit
+
+class FeedTableVC: UITableViewController {
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath)
+    }
+}
